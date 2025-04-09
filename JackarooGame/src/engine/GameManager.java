8bfbf94f721c3d6b0ec39b10c6a86fc85296f91c
Ahmed D. Haggag <157414131+ahmeddhaggag@users.noreply.github.com>
2025-04-10 00:10:08 +0200

    Update

--- JackarooGame/src/engine/GameManager.java
+++ JackarooGame/src/engine/GameManager.java
@@ -1,5 +1,15 @@
 package engine;
 
+import exception.*;
+import model.Colour;
+import model.card.standard.Marble;
+
 public interface GameManager {
 
+	void sendHome(Marble marble);
+	void fieldMarble() throws CannotFieldException, IllegalDestroyException;
+	void discardCard(Colour colour) throws CannotDiscardException;
+	void discardCard() throws CannotDiscardException;
+	Colour getActivePlayerColour();
+	Colour getNextPlayerColour();
 }
